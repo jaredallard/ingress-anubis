850b9f707cbc1038aa886bc6f7e15dd411e40792
Jared Allard <jaredallard@users.noreply.github.com>
2025-03-21 13:20:28 -0700

    build: get version directly from goreleaser

--- scripts/publish-helm.sh
+++ scripts/publish-helm.sh
@@ -12,8 +12,13 @@ for cmd in "${required_tools[@]}"; do
 	fi
 done
 
+NEXT_VERSION=${1:-}
+if [[ "$NEXT_VERSION" == "" ]]; then
+	echo "Missing version"
+	exit 1
+fi
+
 pushd "$REPO_ROOT/deploy/charts/ingress-anubis" >/dev/null
-NEXT_VERSION=$(mise run next-version 2>/dev/null)
 yq e -i '.version = "'"${NEXT_VERSION//v/}"'"' Chart.yaml
 yq e -i '.appVersion = "'"${NEXT_VERSION//v/}"'"' Chart.yaml
 
